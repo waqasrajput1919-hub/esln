9c2e4b5d588186d269b651ff8a69954bfe7dd9f9
Rana Waqas <waqasrajput1919@gmail.com>
2025-09-04 05:09:42 -0700

    change2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ const page = () => {
   return (
     <div>
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
+  < Image 
+   src="/nnn.jpg"
+   alt="my profile"
+   width={300}
+   height={300}
+/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
